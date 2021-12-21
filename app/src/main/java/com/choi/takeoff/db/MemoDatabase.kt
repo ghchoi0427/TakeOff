4bb9c08d0ab685e915dda164d55cfa56e2808f33
ghchoi0427 <ghchoi0427@gmail.com>
2021-12-21 21:45:49 +0900

    feat: MemoDatabase 생성

---
+++ app/src/main/java/com/choi/takeoff/db/MemoDatabase.kt
@@ -0,0 +1,50 @@
+package com.choi.takeoff.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.choi.takeoff.db.dao.MemoDao
+import com.choi.takeoff.db.entity.Memo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Database(entities = [Memo::class], version = 1)
+abstract class MemoDatabase : RoomDatabase() {
+    abstract fun memoDao(): MemoDao
+
+    private class MemoDatabaseCallback(
+        private val scope: CoroutineScope,
+    ) : RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    //TODO: pre-fill database
+                }
+            }
+        }
+    }
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: MemoDatabase? = null
+
+        fun getDatabase(
+            context: Context, scope: CoroutineScope,
+        ): MemoDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(context.applicationContext,
+                    MemoDatabase::class.java,
+                    "memo_database")
+                    .addCallback(MemoDatabaseCallback(scope))
+                    .allowMainThreadQueries()
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
