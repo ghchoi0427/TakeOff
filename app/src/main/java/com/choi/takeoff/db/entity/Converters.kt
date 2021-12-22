3e10cd79c31011e2dd0a5d607da41f3189dfe452
ghchoi0427 <ghchoi0427@gmail.com>
2021-12-22 11:16:12 +0900

    feat: Converters 생성

---
+++ app/src/main/java/com/choi/takeoff/db/entity/Converters.kt
@@ -0,0 +1,41 @@
+package com.choi.takeoff.db.entity
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.ByteArrayOutputStream
+import java.util.*
+
+
+class Converters {
+
+    @TypeConverter
+    fun toBitmap(bytes: ByteArray): Bitmap {
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+    }
+
+    @TypeConverter
+    fun fromBitmap(bmp: Bitmap): ByteArray {
+        val outputStream = ByteArrayOutputStream()
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+        return outputStream.toByteArray()
+    }
+
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time?.toLong()
+    }
+
+    @TypeConverter
+    fun listToJson(value: List<String>?): String = Gson().toJson(value)
+
+    @TypeConverter
+    fun jsonToList(value: String) = Gson().fromJson(value, Array<String>::class.java).toList()
+}
