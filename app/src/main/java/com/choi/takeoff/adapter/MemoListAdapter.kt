f00b88b5dfe19d56541e58926644bbdd9b39315e
ghchoi0427 <ghchoi0427@gmail.com>
2021-12-22 11:21:19 +0900

    feat: 리사이클러뷰 어댑터 생성

---
+++ app/src/main/java/com/choi/takeoff/adapter/MemoListAdapter.kt
@@ -0,0 +1,50 @@
+package com.choi.takeoff.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.choi.takeoff.R
+import com.choi.takeoff.db.entity.Memo
+
+class MemoListAdapter : ListAdapter<Memo, MemoListAdapter.MemoViewHolder>(MemosComparator()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemoViewHolder {
+        return MemoViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: MemoViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(current.content)
+    }
+
+    class MemoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val memoItemView: TextView = itemView.findViewById(R.id.textView)
+
+        fun bind(text: String?) {
+            memoItemView.text = text.toString()
+        }
+
+        companion object {
+            fun create(parent: ViewGroup): MemoViewHolder {
+                val view: View = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.recyclerview_item, parent, false)
+                return MemoViewHolder(view)
+            }
+        }
+    }
+
+    class MemosComparator : DiffUtil.ItemCallback<Memo>() {
+        override fun areItemsTheSame(oldItem: Memo, newItem: Memo): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Memo, newItem: Memo): Boolean {
+            return oldItem.rowid == newItem.rowid
+        }
+    }
+
+}
