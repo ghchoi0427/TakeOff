448cf76581ff7dad2824382d7e3413561e1af541
ghchoi0427 <ghchoi0427@gmail.com>
2021-12-23 14:47:23 +0900

    fix: type migration

--- app/src/main/java/com/choi/takeoff/db/entity/Memo.kt
+++ app/src/main/java/com/choi/takeoff/db/entity/Memo.kt
@@ -1,16 +1,17 @@
 package com.choi.takeoff.db.entity
 
-import android.graphics.Bitmap
-import androidx.room.*
-import java.time.LocalDateTime
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Fts4
+import androidx.room.PrimaryKey
 
 @Fts4
 @Entity(tableName = "memo")
 data class Memo(
     @PrimaryKey @ColumnInfo(name = "rowid") var rowid: Int,
     @ColumnInfo(name = "content") var content: String?,
-    @ColumnInfo(name = "picture") var picture: Bitmap?,
-    @ColumnInfo(name = "time") var time: LocalDateTime?,
+    @ColumnInfo(name = "picture") var picture: String?,
+    @ColumnInfo(name = "time") var time: String?,
     @ColumnInfo(name = "mood") var mood: Int?,
     @ColumnInfo(name = "tags") var tags: List<String>?,
 )
