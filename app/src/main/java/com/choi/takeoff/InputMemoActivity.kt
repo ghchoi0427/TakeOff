1644bfb428f7be0dc5bf561a3dc75f3da8f66d9f
ghchoi0427 <ghchoi0427@gmail.com>
2021-12-23 14:52:07 +0900

    feat: 사진 넣기 기능 추가

--- app/src/main/java/com/choi/takeoff/InputMemoActivity.kt
+++ app/src/main/java/com/choi/takeoff/InputMemoActivity.kt
@@ -2,6 +2,9 @@ package com.choi.takeoff
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+import android.net.Uri
 import android.os.Bundle
 import android.text.TextUtils
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +13,8 @@ import com.choi.takeoff.databinding.ActivityInputMemoBinding
 class InputMemoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityInputMemoBinding
+    val SELECT_PICTURE = 200
+    var imageUri: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +32,7 @@ class InputMemoActivity : AppCompatActivity() {
             } else {
                 val content = editText.text.toString()
                 replyIntent.putExtra(EXTRA_REPLY, content)
+                    .putExtra(EXTRA_PICTURE, imageUri)
                 setResult(Activity.RESULT_OK, replyIntent)
             }
             finish()
@@ -38,9 +44,49 @@ class InputMemoActivity : AppCompatActivity() {
             setResult(Activity.RESULT_CANCELED, replyIntent)
             finish()
         }
+
+        val buttonPicture = binding.buttonUploadPicture
+        buttonPicture.setOnClickListener {
+            val i = Intent()
+            i.type = "image/*"
+            i.action = Intent.ACTION_OPEN_DOCUMENT
+            startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK && requestCode == SELECT_PICTURE) {
+            imageUri = data?.data.toString()
+            val imagePreview = binding.imagePreview
+            imagePreview.setImageURI(Uri.parse(imageUri))
+
+            checkPermission(Uri.parse(imageUri))
+        }
+    }
+
+    private fun checkPermission(uri: Uri) {
+
+        intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+        intent.flags = Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+
+        val resInfoList: List<ResolveInfo> =
+            packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
+        resInfoList.forEach {
+            val packageName: String = it.activityInfo.packageName
+            applicationContext.grantUriPermission(packageName,
+                uri,
+                Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            applicationContext.grantUriPermission(packageName,
+                uri,
+                Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+        }
+
     }
 
     companion object {
         const val EXTRA_REPLY = "com.choi.takeoff.memolistsql.REPLY"
+        const val EXTRA_PICTURE = "com.choi.takeoff.memolistsql.PICTURE"
     }
 }
