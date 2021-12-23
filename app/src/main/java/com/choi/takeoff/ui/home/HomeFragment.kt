b9cd28b46c751262f2d9004e3659e4fbb91f8b50
ghchoi0427 <ghchoi0427@gmail.com>
2021-12-23 14:57:30 +0900

    feat: memo에 글, 사진 추가 기능

--- app/src/main/java/com/choi/takeoff/ui/home/HomeFragment.kt
+++ app/src/main/java/com/choi/takeoff/ui/home/HomeFragment.kt
@@ -1,29 +1,45 @@
 package com.choi.takeoff.ui.home
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.choi.takeoff.GlobalApplication
+import com.choi.takeoff.InputMemoActivity
 import com.choi.takeoff.R
+import com.choi.takeoff.adapter.MemoListAdapter
 import com.choi.takeoff.databinding.FragmentHomeBinding
+import com.choi.takeoff.db.entity.Memo
+import com.choi.takeoff.ui.newmemo.NewMemoViewModel
+import com.choi.takeoff.ui.newmemo.NewMemoViewModelFactory
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.time.LocalDateTime
+import kotlin.random.Random
 
 class HomeFragment : Fragment() {
+
+    private val newMemoFragmentRequestCode = 1
+    private val newMemoViewModel: NewMemoViewModel by viewModels {
+        NewMemoViewModelFactory((activity?.application as GlobalApplication).repository)
+    }
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
@@ -31,11 +47,51 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        val recyclerView = root.findViewById<RecyclerView>(R.id.recyclerview_memo)
+        val adapter = MemoListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+
+        newMemoViewModel.allMemos.observe(this.viewLifecycleOwner, { memos ->
+            memos?.let {
+                adapter.submitList(it)
+            }
         })
+
+        val fab = root.findViewById<FloatingActionButton>(R.id.fab_add)
+        fab.setOnClickListener {
+            val intent = Intent(activity, InputMemoActivity::class.java)
+            startActivityForResult(intent, newMemoFragmentRequestCode)
+        }
         return root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        var content: String? = ""
+        var imageUri: String? = null
+
+        if (requestCode == newMemoFragmentRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.getStringExtra(InputMemoActivity.EXTRA_REPLY)?.let { reply ->
+                content = reply
+            }
+            data?.getStringExtra(InputMemoActivity.EXTRA_PICTURE)
+                ?.let { reply ->
+                    imageUri = reply
+                }
+
+            val memo = Memo(Random.nextInt(0, 2147483647),
+                content,
+                imageUri,
+                LocalDateTime.now().toString(),
+                null,
+                null)
+            //TODO: mood, tags 추가
+            newMemoViewModel.insert(memo)
+        } else {
+            Toast.makeText(context, R.string.input_canceled, Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {
