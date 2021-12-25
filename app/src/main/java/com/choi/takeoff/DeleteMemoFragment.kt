4eb7dfaaa85bb900fa08cc732b0a92f88897a35d
ghchoi0427 <ghchoi0427@gmail.com>
2021-12-25 16:00:26 +0900

    feat: dialog에서 삭제기능 구현

--- app/src/main/java/com/choi/takeoff/DeleteMemoFragment.kt
+++ app/src/main/java/com/choi/takeoff/DeleteMemoFragment.kt
@@ -3,23 +3,25 @@ package com.choi.takeoff
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
-import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import com.choi.takeoff.ui.memo.NewMemoViewModel
+import com.choi.takeoff.ui.memo.NewMemoViewModelFactory
 
-class DeleteMemoFragment : DialogFragment() {
+class DeleteMemoFragment(private val rowId: Int) : DialogFragment() {
+
+    private val newMemoViewModel: NewMemoViewModel by viewModels {
+        NewMemoViewModelFactory((activity?.application as GlobalApplication).repository)
+    }
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
             builder.setMessage(R.string.dialog_confirm_delete_memo)
                 .setPositiveButton(R.string.confirm) { dialog, id ->
-                    Toast.makeText(context,
-                        "confirmed",
-                        Toast.LENGTH_SHORT).show()
+                    newMemoViewModel.deleteMemoById(rowId)
                 }
                 .setNegativeButton(R.string.cancel) { dialog, id ->
-                    Toast.makeText(context,
-                        "canceled",
-                        Toast.LENGTH_SHORT).show()
                 }
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
