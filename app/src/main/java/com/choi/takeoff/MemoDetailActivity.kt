34630a99f5a752f0e517dbc5711fe865011bc9ec
ghchoi0427 <ghchoi0427@gmail.com>
2021-12-25 15:46:16 +0900

    feat: 이미지 설정 예외처리

--- app/src/main/java/com/choi/takeoff/MemoDetailActivity.kt
+++ app/src/main/java/com/choi/takeoff/MemoDetailActivity.kt
@@ -1,15 +1,15 @@
-package com.choi
+package com.choi.takeoff
 
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.choi.takeoff.GlobalApplication
 import com.choi.takeoff.adapter.MemoListAdapter
 import com.choi.takeoff.databinding.ActivityMemoDetailBinding
 import com.choi.takeoff.db.entity.Memo
-import com.choi.takeoff.ui.newmemo.NewMemoViewModel
-import com.choi.takeoff.ui.newmemo.NewMemoViewModelFactory
+import com.choi.takeoff.ui.memo.NewMemoViewModel
+import com.choi.takeoff.ui.memo.NewMemoViewModelFactory
 
 class MemoDetailActivity : AppCompatActivity() {
 
@@ -26,10 +26,15 @@ class MemoDetailActivity : AppCompatActivity() {
         val memoId: Int = intent.getIntExtra(MemoListAdapter.MEMO_OBJECT_ID, -1)
         val memo: Memo = newMemoViewModel.memoById(memoId)
 
-        try {
-            viewMemoContent.imageMemo.setImageURI(Uri.parse(memo.picture))
-        } catch (e: Exception) {
-            e.printStackTrace()
+        if (memo.picture == null) {
+            viewMemoContent.imageMemo.visibility = View.GONE
+        } else {
+            viewMemoContent.imageMemo.visibility = View.VISIBLE
+            try {
+                viewMemoContent.imageMemo.setImageURI(Uri.parse(memo.picture))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
         viewMemoContent.textMemoDetailTime.text = memo.time
         viewMemoContent.textMemoDetailContent.text = memo.content
