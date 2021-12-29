4763cb3ca6e5480e7027e52f2e795a1beb28bda2
ghchoi0427 <ghchoi0427@gmail.com>
2021-12-30 00:07:15 +0900

    feat: 앱별 저장소 접근 유틸 메서드 생성

---
+++ app/src/main/java/com/choi/takeoff/util/StorageManager.kt
@@ -0,0 +1,25 @@
+package com.choi.takeoff.util
+
+import android.content.Context
+
+class StorageManager {
+
+    companion object {
+        fun writeFile(filename: String?, byteArray: ByteArray?, context: Context) {
+            context.openFileOutput(filename, Context.MODE_PRIVATE).use {
+                it.write(byteArray)
+            }
+        }
+
+        fun readFile(filename: String?, context: Context): ByteArray {
+            return context.openFileInput(filename).readBytes()
+        }
+
+        fun deleteFile(filename: String?, context: Context) {
+            context.openFileOutput(filename, Context.MODE_PRIVATE).use {
+                it.write(null)
+            }
+        }
+
+    }
+}
